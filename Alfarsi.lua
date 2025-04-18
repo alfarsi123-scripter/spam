e65d19f0e8608216a59596d48b74ffbe0b6eda0d
AlfarsiOnTop <shdlkshdlk5@gmail.com>
2025-04-18 21:58:56 +0400

    Alfarsi.lua

--- Alfarsi.lua
+++ Alfarsi.lua
@@ -1,13 +1,38 @@
---// Visual UI Library
-local Library = loadstring(game:HttpGet("https://pastebin.com/raw/Zz5bSUsL"))()
+-- Load the Visual UI Library
+local Library = loadstring(game:HttpGet('https://raw.githubusercontent.com/VisualRoblox/Roblox/main/UI-Libraries/Visual%20UI%20Library/Source.lua'))()  --  [oai_citation_attribution:0‡GitHub](https://github.com/VisualRoblox/Roblox/blob/main/UI-Libraries/Visual%20UI%20Library/Example.lua?utm_source=chatgpt.com)
 
-local Window = Library:CreateWindow("Alfarsi", Vector2.new(490, 550), Enum.KeyCode.RightControl)
+-- Create the main window
+local Window = Library:CreateWindow(
+    'Alfarsi',                 -- Window title
+    'Roblox',                  -- Game title
+    'Alfarsi',                 -- Splash screen text
+    'rbxassetid://10618928818',-- Splash screen icon
+    false,                     -- Disable themes
+    'AlfarsiConfigs',          -- Config folder name
+    'Default'                  -- Default theme name
+)
 
-local Tab = Window:CreateTab("Main")
-Tab:CreateLabel("Programmed by: unknown")
-Tab:CreateLabel("Discord: 3kp_")
+-- Main tab
+local MainTab = Window:CreateTab(
+    'Main',                    -- Tab title
+    true,                      -- Visible by default
+    'rbxassetid://3926305904', -- Tab icon
+    Vector2.new(524, 44),      -- Tab RectOffset
+    Vector2.new(36, 36)        -- Tab RectSize
+)
+local MainSection = MainTab:CreateSection('Info')
+MainSection:CreateLabel('Programmed by: unknown')
+MainSection:CreateLabel('Discord: 3kp_')
 
-local Other = Window:CreateTab("Other")
-Other:CreateButton("Launch Infinite Yield", function()
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source"))()
+-- Other tab
+local OtherTab = Window:CreateTab(
+    'Other',                   -- Tab title
+    true,                      -- Visible by default
+    'rbxassetid://3926305904', -- Tab icon
+    Vector2.new(524, 44),      -- Tab RectOffset
+    Vector2.new(36, 36)        -- Tab RectSize
+)
+local OtherSection = OtherTab:CreateSection('Scripts')
+OtherSection:CreateButton('Launch Infinite Yield', function()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'))()
 end)
