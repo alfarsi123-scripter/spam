5bdbbea79ea70bf0d9b756bc530e301505d330cd
AlfarsiOnTop <shdlkshdlk5@gmail.com>
2025-04-18 22:02:58 +0400

    Alfarsi.lua

--- Alfarsi.lua
+++ Alfarsi.lua
@@ -1,16 +1,19 @@
 -- Load the Visual UI Library
 local Library = loadstring(game:HttpGet('https://raw.githubusercontent.com/VisualRoblox/Roblox/main/UI-Libraries/Visual%20UI%20Library/Source.lua'))()  --  [oai_citation_attribution:0‡GitHub](https://github.com/VisualRoblox/Roblox/blob/main/UI-Libraries/Visual%20UI%20Library/Example.lua?utm_source=chatgpt.com)
 
--- Create the main window
+-- Create the main window with the "Discord" theme
 local Window = Library:CreateWindow(
     'Alfarsi',                 -- Window title
     'Roblox',                  -- Game title
-    'Alfarsi',                 -- Splash screen text
-    'rbxassetid://10618928818',-- Splash screen icon
-    false,                     -- Disable themes
+    'Alfarsi',                 -- Splash/Text on load
+    'rbxassetid://10618928818',-- Splash icon
+    false,                     -- Disable built‑in theming toggle
     'AlfarsiConfigs',          -- Config folder name
-    'Default'                  -- Default theme name
+    'Discord'                  -- Default theme name (see list below)  [oai_citation_attribution:1‡GitHub](https://github.com/VisualRoblox/Roblox/blob/main/UI-Libraries/Visual%20UI%20Library/Themes.md)
 )
+
+-- (Optional) Change the key that toggles the UI
+Window.ToggleKey = Enum.KeyCode.RightControl  -- You can set this to e.g. Enum.KeyCode.RightShift
 
 -- Main tab
 local MainTab = Window:CreateTab(
@@ -29,8 +32,8 @@ local OtherTab = Window:CreateTab(
     'Other',                   -- Tab title
     true,                      -- Visible by default
     'rbxassetid://3926305904', -- Tab icon
-    Vector2.new(524, 44),      -- Tab RectOffset
-    Vector2.new(36, 36)        -- Tab RectSize
+    Vector2.new(524, 44),
+    Vector2.new(36, 36)
 )
 local OtherSection = OtherTab:CreateSection('Scripts')
 OtherSection:CreateButton('Launch Infinite Yield', function()
